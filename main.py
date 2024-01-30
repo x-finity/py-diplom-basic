c76469880203267b4e9357fb844311c07df0f050
XFinity <x.finity777@gmail.com>
2024-01-30 13:16:32 +0300

    create methods for classes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 import xF_vkapi as vk
 import xF_yadapi as ya
 
@@ -10,6 +11,27 @@ def load_cfg(filename = '__pycache__/config.json'):
 vk_token = load_cfg()['vk_token']
 ya_token = load_cfg()['ya_token']
 
-vk_client = vk.VKAPIClient(vk_token, '86301318')
+#user_id = input('user_id: ') #4957073 86301318
+vk_client = vk.VKAPIClient(vk_token)
 ya_client = ya.YandexDiskAPIClient(ya_token)
-print(vk_client.get_proile_photos_info())
+
+# pprint(vk_client.get_proile_photos_info())
+def get_vk_photo_url(quantity = 5):
+    photos = vk_client.get_proile_photos_info().get('response', {})['items']
+    # pprint(photos)
+    urls = []
+    n = 0
+    if len(photos) < quantity:
+        quantity = len(photos)
+    while n < quantity:
+        urls.append(photos[n]['sizes'][-1]['url'])
+        n += 1
+    return urls
+
+# print(get_vk_photo_url())
+test_img_url = 'https://cdnn21.img.ria.ru/images/148651/95/1486519535_0:55:1100:1155_1920x0_80_0_0_29a1904b98600954160c6658aa820868.jpg'
+# ya_client.create_folder(2)
+# ya_client.upload_file(test_img_url, 'test_img.jpg')
+
+file_path = '123/test_img.jpg'
+print(file_path.split('/')[0])
--- xF_yadapi.py
+++ xF_yadapi.py
@@ -6,5 +6,31 @@ def test_function():
     print('yad test function')
 
 class YandexDiskAPIClient:
+    base_url = 'https://cloud-api.yandex.net/v1/disk'
     def __init__(self, token):
         self.token = token
+    
+    def get_headers(self):
+        return {
+            'Authorization': self.token
+        }
+
+    def create_folder(self, folder_name):
+        response = requests.put(f'{self.base_url}/resources', headers=self.get_headers(), params={'path': folder_name})
+        if response.status_code == 409:
+            print(f'Папка {folder_name} уже существует')
+        # дополнить обработчик ошибок/статусов
+        elif response.status_code == 201:
+            print(f'Папка {folder_name} создана')
+
+    def upload_file_by_url(self, file_url, file_name):
+        api_upload_url = f'{self.base_url}/resources/upload'
+        response = requests.post(api_upload_url, headers=self.get_headers(), params={'path': file_name, 'url': file_url})
+        if response.status_code == 202:
+            print(f'Файл {file_name} успешно загружен')
+        if response.status_code == 409:
+            print(f'Указанного пути {file_name} не существует. Cоздаем папку')
+            self.create_folder(file_name.split('/')[0])
+            self.upload_file_by_url(file_url, file_name)
+        # pprint(response.json())
+
